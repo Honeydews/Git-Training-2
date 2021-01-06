83cbcf1c13e9e63f01411495948bb064c7f5cb09
Honeydews <highcore444@gmail.com>
2021-01-06 23:55:19 +0900

    NewScript.cs 생성
    GameObject와 Start, ChangeActiveGo2 함수 생성.

---
+++ NewScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TypeMedicine : MonoBehaviour
+{
+    public GameObject Go;
+    public GameObject Go2;
+
+    public void Start()
+    {
+        go.SetActive(true);
+    }
+
+    public void ChangeActiveGo2()
+    {
+        go2.SetActive(true);
+    }
+
+}
